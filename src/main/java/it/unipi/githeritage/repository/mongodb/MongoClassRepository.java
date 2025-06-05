3cf46c00ad4eeb037f80f96c264f3959e1c3c00d
andrea <a.dimatteo5@studenti.unipi.it>
2025-06-05 17:33:37 +0200

    :memo: imported model.mongodb.Class declaration

--- src/main/java/it/unipi/githeritage/repository/mongodb/MongoClassRepository.java
+++ src/main/java/it/unipi/githeritage/repository/mongodb/MongoClassRepository.java
@@ -1,7 +1,8 @@
 package it.unipi.githeritage.repository.mongodb;
 
-import it.unipi.githeritage.model.mongodb.Project;
+import it.unipi.githeritage.model.mongodb.Class;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MongoClassRepository extends MongoRepository<Project, String> {
+public interface MongoClassRepository extends MongoRepository<Class, String> {
+
 }
--- src/main/java/it/unipi/githeritage/repository/mongodb/MongoUserRepository.java
+++ src/main/java/it/unipi/githeritage/repository/mongodb/MongoUserRepository.java
@@ -1,11 +1,10 @@
 package it.unipi.githeritage.repository.mongodb;
 
+import it.unipi.githeritage.model.mongodb.Project;
 import it.unipi.githeritage.model.mongodb.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MongoUserRepository extends MongoRepository<User, String> {
-
 }
